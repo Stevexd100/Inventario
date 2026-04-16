946335f9ea7e7a42d4b0ad814409ac274df36655
Stevexd100 <steve.lopez@upaep.mx>
2026-04-16 07:57:57 -0600

    Update Inventario.cs

--- Inventario.cs
+++ Inventario.cs
@@ -1,1 +1,30 @@
+using System ;
 
+public class Inventario
+{
+  public void AgregarProductos(nombre,stock)
+  {
+    
+  } 
+
+  public void ListarProductos()
+  {
+    
+  }
+
+    public void BuscarProducto(nombre)
+  {
+    
+  } 
+
+    public void ActualizarStock(id,nuevoStock)
+  {
+    
+  } 
+
+    public void EliminarProducto(id)
+  {
+    
+  } 
+
+}  
